26a5410cb625d8fb9a43c6ad8b6920148c31d101
Neira Cehic <neira.cehic@student.etf.unibl.org>
2025-03-13 21:58:33 +0100

    Issue #53 : Create Module for Integrating All Modules in One System - Added comments and improved coding standards.

--- hardware/design/sonar_controller.vhd
+++ hardware/design/sonar_controller.vhd
@@ -1,25 +1,24 @@
------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
 -- Faculty of Electrical Engineering
 -- PDS 2024
 -- https://github.com/etf-unibl/fpga-sonar
------------------------------------------------------------------------------
--- unit name:     Servo_Controller
+---------------------------------------------------------------------------------
+-- unit name:     Sonar controller module
 --
 -- description:
 --
---   This file implements the servo controller module.
---   The module generates a PWM signal for controlling an RC servo motor.
---   It updates the servo angle using a triangular waveform.
---   The angle is updated every g_UPDATE_PERIOD PWM periods.
+-- This module integrates a servo motor, an ultrasonic sensor, and a VGA
+-- display to create a real-time object detection and visualization system.
+-- It generates a start signal for the ultrasonic sensor, produces a PWM
+-- signal to control the servo angle, and measures the distance to detected
+-- objects. The measured distance and current servo angle are then sent to
+-- the VGA display, where the detected object is graphically represented.
 --
---   The module instantiates the servo module, which receives the servo angle and
---   generates the PWM output signal.
------------------------------------------------------------------------------
--- Copyright (c) 2023 Faculty of Electrical Engineering
------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
+-- Copyright (c) 2024 Faculty of Electrical Engineering
+---------------------------------------------------------------------------------
 -- The MIT License
------------------------------------------------------------------------------
-
+---------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -29,128 +28,125 @@ entity sonar_controller is
     g_UPDATE_PERIOD : integer := 5  --! Number of PWM periods between angle updates.
   );
   port (
-    clk_i          : in  std_logic;  --! Clock signal (50 MHz).
-    rst_i          : in  std_logic;  --! Asynchronous reset (active low).
-    servo_pwm_o    : out std_logic;   --! PWM output to servo module.
-    vclk_o         : out std_logic; --! VGA pixel clock
+    clk_i          : in  std_logic; --! Clock signal (50 MHz)
+    rst_i          : in  std_logic; --! Asynchronous reset (active low)
+    servo_pwm_o    : out std_logic; --! PWM output to servo module
+    vclk_o         : out std_logic; --! VGA clock
     hsync_o        : out std_logic; --! Horizontal sync signal
     vsync_o        : out std_logic; --! Vertical sync signal
     blank_o        : out std_logic; --! Blanking signal
     sync_o         : out std_logic; --! Sync signal (always '0')
     r_o            : out std_logic_vector(7 downto 0); --! Red color (8 bits)
     g_o            : out std_logic_vector(7 downto 0); --! Green color (8 bits)
-    b_o            : out std_logic_vector(7 downto 0);  --! Blue color (8 bits)
-    echo_i         : in  std_logic;                     --! Echo input from HC-SR04 sensor.
-    trigger_o      : out std_logic                     --! Trigger output to HC-SR04.
+    b_o            : out std_logic_vector(7 downto 0); --! Blue color (8 bits)
+    echo_i         : in  std_logic;                    --! Echo input from HC-SR04 sensor
+    trigger_o      : out std_logic                     --! Trigger output to HC-SR04
 
   );
 end sonar_controller;
 
 architecture arch of sonar_controller is
 
-  --! @brief Constant declarations.
+  --! @brief Constant declarations
   --! @details
-  --!   Defines clock frequency, PWM frequency, and related timing parameters.
-  constant c_CLK_HZ            : integer := 50000000;              --! Clock frequency: 50 MHz.
-  constant c_PULSE_HZ          : integer := 50;                    --! PWM frequency: 50 Hz.
-  constant c_PWM_PERIOD_CYCLES : integer := c_CLK_HZ / c_PULSE_HZ; --! Number of clock cycles per PWM period.
+  --! Defines clock frequency, PWM frequency, and related timing parameters
+  constant c_CLK_HZ            : integer := 50000000;              --! Clock frequency: 50 MHz
+  constant c_PULSE_HZ          : integer := 50;                    --! PWM frequency: 50 Hz
+  constant c_PWM_PERIOD_CYCLES : integer := c_CLK_HZ / c_PULSE_HZ; --! Number of clock cycles per PWM period
 
-  constant c_MIN_ANGLE : integer := 0;    --! Minimum servo angle.
-  constant c_MAX_ANGLE : integer := 180;  --! Maximum servo angle.
-  constant c_FULL_CYCLE  : integer := 2 * c_MAX_ANGLE;  --! Full cycle for the triangular waveform (e.g., 358).
+  constant c_MIN_ANGLE : integer := 0;    --! Minimum servo angle
+  constant c_MAX_ANGLE : integer := 180;  --! Maximum servo angle
+  constant c_FULL_CYCLE  : integer := 2 * c_MAX_ANGLE;  --! Full cycle for the triangular waveform (e.g., 358)
 
   --! @brief Signal declarations.
-  signal servo_angle        : unsigned(7 downto 0) := (others => '0');         --! Current servo angle.
-  signal pwm_period_counter : integer range 0 to c_PWM_PERIOD_CYCLES - 1 := 0; --! Counter for clock cycles within a PWM period.
-  signal update_counter     : integer range 0 to g_UPDATE_PERIOD - 1 := 0;     --! Counter for PWM periods between angle updates.
-  signal phase              : integer range 0 to c_FULL_CYCLE := 0;   --! Current phase of the triangular waveform.
-  signal hpos  : integer; --! Horizontal position
-  signal vpos  : integer; --! Vertical position
-  signal de    : std_logic; --! Data enable signal
-  signal distance_cm : std_logic_vector(8 downto 0);
-  signal done         : std_logic;                     --! Measurement complete indicator.
-  signal object_found : std_logic;                      --! '1' if an object is detected.
-  signal sonar_start        : std_logic;                     --! Start measurement signal (active low).
-  
+  signal servo_angle        : unsigned(7 downto 0) := (others => '0');         --! Current servo angle
+  signal pwm_period_counter : integer range 0 to c_PWM_PERIOD_CYCLES - 1 := 0; --! PWM cycle counter
+  signal update_counter     : integer range 0 to g_UPDATE_PERIOD - 1 := 0;     --! Counter for PWM periods between angle updates
+  signal phase              : integer range 0 to c_FULL_CYCLE := 0;            --! Current phase
+  signal hpos               : integer;   --! Horizontal position
+  signal vpos               : integer;   --! Vertical position
+  signal de                 : std_logic; --! Display enable signal
+  signal distance_cm        : std_logic_vector(8 downto 0); --! Measured distance in cm
+  signal done               : std_logic; --! Measurement complete indicator.
+  signal object_found       : std_logic; --! '1' if an object is detected.
+  signal sonar_start        : std_logic; --! Start measurement signal (active low)
+
   component vga_controller is
     port(
-      clk_i   : in  std_logic;
-      rst_i   : in  std_logic;
-      vclk_o  : out std_logic;
-      hsync_o : out std_logic;
-      vsync_o : out std_logic;
-      blank_o : out std_logic;
-      sync_o  : out std_logic;
-      hpos_o  : out integer;
-      vpos_o  : out integer;
-      de_o    : out std_logic
+      clk_i   : in  std_logic; --! Clock signal
+      rst_i   : in  std_logic; --! Reset signal
+      vclk_o  : out std_logic; --! VGA pixel clock
+      hsync_o : out std_logic; --! Horizontal sync signal
+      vsync_o : out std_logic; --! Vertical sync signal
+      blank_o : out std_logic; --! Blanking signal
+      sync_o  : out std_logic; --! Sync signal (always '0')
+      hpos_o  : out integer;   --! Current horizontal position
+      vpos_o  : out integer;   --! Current vertical position
+      de_o    : out std_logic  --! Display enable signal
     );
   end component;
 
   component draw_logic is
     port(
-      clk_i      : in  std_logic;
-      rst_i      : in  std_logic;
-      hpos_i     : in  integer;
-      vpos_i     : in  integer;
-      de_i       : in  std_logic;
-      angle_i    : in  unsigned(7 downto 0);
-      distance_i : in  std_logic_vector(8 downto 0);
-      r_o        : out std_logic_vector(7 downto 0);
-      g_o        : out std_logic_vector(7 downto 0);
-      b_o        : out std_logic_vector(7 downto 0)
+      clk_i      : in  std_logic; --! Clock signal
+      rst_i      : in  std_logic; --! Reset signal
+      hpos_i     : in  integer;   --! Horizontal position
+      vpos_i     : in  integer;   --! Vertical position
+      de_i       : in  std_logic; --! Display enable signal
+      angle_i    : in  unsigned(7 downto 0);         --! Angle input (0-179)
+      distance_i : in  std_logic_vector(8 downto 0); --! Distance input (2-400)
+      r_o        : out std_logic_vector(7 downto 0); --! Red color output
+      g_o        : out std_logic_vector(7 downto 0); --! Green color output
+      b_o        : out std_logic_vector(7 downto 0)  --! Blue color output
     );
   end component;
   --! @brief Component declaration for the Ultrasonic_Sensor.
   component Ultrasonic_Sensor is
     port(
-      clk_i          : in  std_logic;
-      rst_i          : in  std_logic;
-      start_i        : in  std_logic;
-      echo_i         : in  std_logic;
-      trigger_o      : out std_logic;
-      done_o         : out std_logic;
-      distance_cm_o  : out std_logic_vector(8 downto 0);
-      object_found_o : out std_logic
+      clk_i          : in  std_logic; --! Clock signal
+      rst_i          : in  std_logic; --! Reset signal
+      start_i        : in  std_logic; --! Start measurement signal (active low)
+      echo_i         : in  std_logic; --! Echo input from HC-SR04 sensor
+      trigger_o      : out std_logic; --! Trigger output to HC-SR04
+      done_o         : out std_logic; --! Measurement complete indicator
+      distance_cm_o  : out std_logic_vector(8 downto 0); --! Computed distance in cm
+      object_found_o : out std_logic                     --! '1' if an object is detected.
     );
   end component;
 
   component rc_servo is
-  port (
-    clk_i   : in  std_logic;              --! Clock signal (50 MHz).
-    rst_i   : in  std_logic;              --! Asynchronous reset signal (active low).
-    angle_i : in  unsigned(7 downto 0);   --! Servo angle input (0 to 179).
-    pwm_o   : out std_logic               --! PWM output signal.
+    port (
+    clk_i   : in  std_logic;            --! Clock signal
+    rst_i   : in  std_logic;            --! Asynchronous reset signal (active low)
+    angle_i : in  unsigned(7 downto 0); --! Servo angle input (0 to 179)
+    pwm_o   : out std_logic             --! PWM output signal
   );
-end component;
-  begin
+  end component;
+begin
 
-  -------------------------------------------------------------------------
-  --! @brief Main process for updating the servo angle.
+  -------------------------------------------------------------------------------
+  --! @brief Main process for controlling the sonar
   --! @details
-  --!   This process updates the PWM period counter and, upon completion of a PWM period,
-  --!   increments the update counter. When the update counter reaches g_UPDATE_PERIOD, it
-  --!   updates the phase of the triangular waveform and computes the new servo angle.
-  --!   The servo angle is computed based on the phase such that it increases linearly
-  --!   to c_MAX_ANGLE and then decreases, forming a triangular waveform.
-  -------------------------------------------------------------------------
-   process(clk_i, rst_i)
+  --! This process controls the servo motor movement and generates the sonar start
+  --! signal at specific intervals.
+  -------------------------------------------------------------------------------
+  process(clk_i, rst_i)
   begin
     if rst_i = '0' then
-      pwm_period_counter <= 0;
+      pwm_period_counter <= 0;                                            --! Reset the PWM period counter
       update_counter     <= 0;
       phase              <= 0;
-      servo_angle        <= to_unsigned(c_MIN_ANGLE, servo_angle'length);
-      sonar_start        <= '0';
+      servo_angle        <= to_unsigned(c_MIN_ANGLE, servo_angle'length); --! Set the servo angle to minimum
+      sonar_start        <= '0';                                          --! Disable the sonar initially
     elsif rising_edge(clk_i) then
       if pwm_period_counter < c_PWM_PERIOD_CYCLES - 1 then
         pwm_period_counter <= pwm_period_counter + 1;
-        sonar_start <= '0';  -- Keep start low when not updating angle.
+        sonar_start <= '0';
       else
         pwm_period_counter <= 0;
         if update_counter < g_UPDATE_PERIOD - 1 then
           update_counter <= update_counter + 1;
-          sonar_start <= '0';
+          sonar_start    <= '0';
         else
           update_counter <= 0;
           if phase < c_FULL_CYCLE then
@@ -158,9 +154,8 @@ end component;
           else
             phase <= 0;
           end if;
-          sonar_start <= '1';  -- Generate start pulse when angle is updated.
+          sonar_start <= '1';
         end if;
-        -- Calculate the servo angle based on the triangular waveform phase.
         if phase <= c_MAX_ANGLE then
           servo_angle <= to_unsigned(phase, servo_angle'length);
         else
@@ -170,12 +165,12 @@ end component;
     end if;
   end process;
 
-  -------------------------------------------------------------------------
+  -------------------------------------------------------------------------------
   --! @brief Instantiation of the servo module.
   --! @details
-  --!   The servo module receives the computed servo angle and generates a PWM signal
-  --!   to control an RC servo motor.
-  -------------------------------------------------------------------------
+  --! The servo module receives the computed servo angle and generates
+  --! a PWM signal to control an RC servo motor.
+  -------------------------------------------------------------------------------
   servo_inst : rc_servo
     port map (
       clk_i   => clk_i,
@@ -183,7 +178,14 @@ end component;
       angle_i => servo_angle,
       pwm_o   => servo_pwm_o
     );
-      -- Instantijacija VGA kontrolera
+  -------------------------------------------------------------------------------
+  --! @brief Instantiation of the VGA module.
+  --! @details
+  --! The VGA module consists of vga_controller and draw_logic.
+  --! The vga_controller is responsible for configuring the VGA display and
+  --! generating synchronization signals, while the draw_logic module renders
+  --! the object in a specific zone based on the angle and distance measurements.
+  -------------------------------------------------------------------------------
   vga_controller_inst : vga_controller
     port map(
       clk_i   => clk_i,
@@ -198,7 +200,6 @@ end component;
       de_o    => de
     );
 
-  -- Instantijacija draw_logic modula
   draw_logic_inst : draw_logic
     port map(
       clk_i      => clk_i,
@@ -212,7 +213,13 @@ end component;
       g_o        => g_o,
       b_o        => b_o
     );
-  --! @brief Instantiation of the Ultrasonic_Sensor component.
+  -------------------------------------------------------------------------------
+  --! @brief Instantiation of the Ultrasonic sensor module
+  --! @details
+  --! The sensor receives a start signal and attempts to detect an object
+  --! based on the trigger signal. If an object is found, it returns an echo
+  --! signal and calculates the distance at which the object was detected.
+  -------------------------------------------------------------------------------
   Ultrasonic_Sensor_inst : Ultrasonic_Sensor
     port map(
       clk_i          => clk_i,
